e5767019619a45b9b36d50b3d0b3e1bd1c91bb46
Alireza Taji <alirezataji6@gmail.com>
2022-10-14 15:37:09 +0330

    feat: remove extra backups

--- cron-runner.js
+++ cron-runner.js
@@ -1,11 +1,14 @@
 const schedule = require('node-schedule')
 const { exec } = require("child_process");
-
+const fs = require('fs')
 module.exports = () => {
     schedule.scheduleJob(process.env.CRON_SPEC_STRING, function() {
+        const files = fs.readdirSync('files')
+        if(files.length > 4) {
+            fs.unlinkSync(`files/${files[1]}`)
+        } 
         const date = new Date()
         const path = `files/${date.getFullYear()}-${date.getMonth()}-${date.getDate()}.sql`
-        console.log(path);
         exec(`docker exec ${process.env.DB_DOCKER_ID} sh -c 'exec mysqldump --databases ${process.env.DB_NAME} -uroot -p${process.env.DB_PASSWORD}' > ${path}`, (error, stdout, stderr) => {
             if (error) {
                 console.log(`error: ${error.message}`);
